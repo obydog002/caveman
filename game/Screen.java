501ab6d0b0d97703faad4dd5445ecffa7eb74ee6
Max Chatfield <maxc7.08@gmail.com>
2021-01-06 15:39:38 +0800

    added a scale in addition to the width and length

--- game/Screen.java
+++ game/Screen.java
@@ -2,22 +2,38 @@ package game;
 
 public class Screen extends Bitmap
 {
-	public Screen(int width, int height)
+	public final int scale;
+	
+	private int unscaled_width;
+	private int unscaled_height;
+	
+	private int inv_height;
+	
+	public Screen(int width, int height, int scale)
 	{
-		super(width, height);
+		super(width*scale, height*scale);
+		this.unscaled_width = width;
+		this.unscaled_height = height;
+		this.scale = scale;
+		
+		inv_height = (unscaled_height - 1)*scale;
 	}
+	
 	public void render(Game game)
 	{
+		
 		draw(game.background,0,0);
-		draw(Art.toolBar,0,864);
+		draw(Art.toolBar,0,inv_height);
 		for (Entity e : game.entities)
 		{
 			draw(e.getImage(),Main.SCALE * e.getX(),Main.SCALE * e.getY());
 		}
-		draw(Art.club,0,864);
+		
+		draw(Art.club,0,inv_height);
 		String clubs = ":" + game.clubs;
-		draw(clubs,32,864,0xff444444);
-		draw(game.levelName,(int)(Main.SCALE *(Main.WIDTH / 2.0 - 0.5*(game.levelName.length() - 1))),864,0xff444444);
+		draw(clubs,32,inv_height,0xff444444);
+		draw(game.levelName,(int)(Main.SCALE *(Main.WIDTH / 2.0 - 0.5*(game.levelName.length() - 1))),inv_height,0xff444444);
+		
 		if (game.state != 2)
 		{
 			darken(.2);
