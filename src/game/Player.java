d7b405f5a603a436351aa482e3eace72b4bdd932
Max <max@Maxs-MacBook-Air.local>
2023-07-18 18:40:11 +0200

    start to improve buffering, still broken tho

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,7 +1,7 @@
 package src.game;
 
 import java.util.Queue;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 import java.awt.image.BufferedImage;
 
@@ -9,13 +9,15 @@ public class Player extends Mob
 {
 	boolean[] keys;
 
-	private Queue<Integer> movementQueue;	
+	private int currentPosInMovementQueue;
+	private ArrayList<KeyEventPair> movementQueue; 
 	private AbstractInput input;
 	public Player(int x, int y, int timeToMove, BufferedImage image, AbstractInput input)
 	{
 		super(x,y,timeToMove,false,true,false,image);
 		this.input = input;
-		this.movementQueue = new LinkedList<>();
+		this.currentPosInMovementQueue = -1;
+		this.movementQueue = new ArrayList<>();
 	}
 	
 	public void tick(Game game)
@@ -41,89 +43,56 @@ public class Player extends Mob
 					e[lastdir].dead = true;
 				}
 			}
-			if (event.is_clicked()) 
+			else if (event.is_key(LogicalKey.LEFT) || event.is_key(LogicalKey.UP) || event.is_key(LogicalKey.RIGHT) || event.is_key(LogicalKey.DOWN)) 
 			{
-				if (event.is_key(LogicalKey.LEFT))
-					movementQueue.add(0);
-				else if (event.is_key(LogicalKey.UP))
-					movementQueue.add(1);
-				else if (event.is_key(LogicalKey.RIGHT))
-					movementQueue.add(2);
-				else if (event.is_key(LogicalKey.DOWN))
-					movementQueue.add(3);
-			}
-			if (event.is_released()) 
-			{
-				Integer nextMove = movementQueue.peek();
-				if (nextMove != null)
-				{
-					if (nextMove == 0 && event.is_key(LogicalKey.LEFT))
-						movementQueue.remove();
-					else if (nextMove == 1 && event.is_key(LogicalKey.UP))
-						movementQueue.remove();
-					else if (nextMove == 2 && event.is_key(LogicalKey.RIGHT))
-						movementQueue.remove();
-					else if (nextMove == 3 && event.is_key(LogicalKey.DOWN))
-						movementQueue.remove();
-				}
+				if (event.is_clicked())
+					movementQueue.add(event);
 			}
 		}
 		
 		if (time == 0) 
 		{
-			Integer potentialMove = -1;
-			while ((potentialMove = movementQueue.poll()) != null) 
+			boolean validMoveMade = false;
+			while (!validMoveMade) 
 			{
-				if (potentialMove == 0) 
+				if (movementQueue.size() > 0) 
 				{
-					if (input.key_held(LogicalKey.LEFT))
-						movementQueue.add(0);		
-				}
-				else if (potentialMove == 1)
-				{
-					if (input.key_held(LogicalKey.UP))
-						movementQueue.add(1);
-				}
-				else if (potentialMove == 2) 
-				{
-					if (input.key_held(LogicalKey.RIGHT))
-						movementQueue.add(2);
-				}
-				else if (potentialMove == 3)
-				{
-					if (input.key_held(LogicalKey.DOWN))
-						movementQueue.add(3);
-				}
-				lastdir = potentialMove;
-				Entity dir[] = checkDirection(game);
-				if (tryMove(game, dir[potentialMove], potentialMove)) 
-				{
-					time = timeToMove;
-					break;
-				}
+					if (currentPosInMovementQueue == -1 || currentPosInMovementQueue >= movementQueue.size())
+						currentPosInMovementQueue = 0;
+	
+					KeyEventPair candidateMove = movementQueue.get(currentPosInMovementQueue);
+					if (candidateMove.is_clicked() || (candidateMove.is_held() && input.key_held(candidateMove.logicalKey)))
+					{
+						int potentialDirection = -1;
+						if (candidateMove.is_key(LogicalKey.LEFT))
+							potentialDirection = 0;
+						else if (candidateMove.is_key(LogicalKey.UP))
+							potentialDirection = 1;
+						else if (candidateMove.is_key(LogicalKey.RIGHT))
+							potentialDirection = 2;
+						else if (candidateMove.is_key(LogicalKey.DOWN))	
+							potentialDirection = 3;				
+						lastdir = potentialDirection;
+						Entity dir[] = checkDirection(game);
+						if (tryMove(game, dir[potentialDirection], potentialDirection))
+						{
+							time = timeToMove;
+							validMoveMade = true;
+						}
+
+						if (candidateMove.is_clicked()) 
+							candidateMove.type = KeyEventPair.KeyEventType.PRESSED; 
+						currentPosInMovementQueue++;
+					}
+					else
+						movementQueue.remove(currentPosInMovementQueue);
+						
 					
+				}
+				else 
+					currentPosInMovementQueue = -1;
 			}
 		}
-		/*else
-		{
-			if (input.key_held(LogicalKey.LEFT))
-				direction = 0;
-			else if (input.key_held(LogicalKey.UP))
-				direction = 1;
-			else if (input.key_held(LogicalKey.RIGHT))
-				direction = 2;
-			else if (input.key_held(LogicalKey.DOWN))
-				direction = 3;
-		
-			if (direction > -1)
-			{
-				time = timeToMove;
-				lastdir = direction;
-				Entity dir[] = checkDirection(game);
-				tryMove(game, dir[direction], direction);
-				direction = -1;
-			}
-		}*/
 	}
 	
 	//overriden
